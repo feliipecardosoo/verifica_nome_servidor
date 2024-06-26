8eb527ced7daf12999af07eaec8ec5b9a7083c1a
feliipecardosoo <fglibino@gmail.com>
2024-06-26 01:54:02 -0300

    retornando IPS

--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"modulo/app"
 	"os"
 )
 
 func main() {
-	fmt.Println("Ponto de partida")
 
 	aplicacao := app.Gerar()
 	if erro := aplicacao.Run(os.Args); erro != nil {
